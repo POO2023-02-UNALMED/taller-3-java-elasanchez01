b4c33a79dd91f8f85fa2e0b94ce3b69d861e374f
elasanchez01 <dasancheza@unal.edu.co>
2023-08-27 02:55:01 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,5 +1,43 @@
 package taller3.televisores;
 
 public class Control {
+	private TV tv;
 
+	public void encender() {
+		tv.turnOn.TV();
+	}
+	
+	public void apagar() {
+		tv.turnOff.TV();
+	}
+	
+	public void upCanal() {
+		tv.canalUp.TV();
+	}
+	
+	public void downCanal() {
+		tv.canalDown.TV();
+	}
+	
+	public void upVolumen() {
+		tv.volumenUp.TV();
+	}
+	
+	public void downVolumen() {
+		tv.volumenDown.TV();	
+	}
+	
+	public void setCanal (int a_canal) {
+		canal = a_canal ;
+	}
+	
+	public void setCanal (int a_volumen) {
+		canal = a_volumen ;
+	}
+	
+	public void enlazar (TV televisor, Control control) {
+		tv = televisor ;
+		this.control = control ;
+	}
 }
+	
